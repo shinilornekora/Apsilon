3a0aab72e467bb76474c4c8b23a6f0f054725884
Shiniasse <shiniasse.andyorl@gmail.com>
2024-11-01 01:34:58 +0000

    [UPDATE]: little java measures

---
+++ java_gateway/demo/src/main/java/com/example/ConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ConsumerApplication {
+    static final String QUEUE_NAME = "requestQueue";
+
+    @Bean
+    public Queue myQueue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @RabbitListener(queues = QUEUE_NAME)
+    public void listen(String message) {
+        System.out.println("Message read from firstQueue : " + message);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
