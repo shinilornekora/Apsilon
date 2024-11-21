0bdf68f8e0f8434777fe9c25aa53c175373c8ce7
Your Name <you@example.com>
2024-11-22 01:13:53 +0300

    [UPDATE]: gateway is set

--- java_gateway/demo/src/main/java/com/example/ConsumerApplication.java
+++ java_gateway/demo/src/main/java/com/example/ConsumerApplication.java
@@ -1,14 +1,30 @@
 package com.example;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import com.example.grpc.MessageServiceGrpc;
+import com.example.grpc.MessageServiceProto.RequestMessage;
 
 @SpringBootApplication
 public class ConsumerApplication {
     static final String QUEUE_NAME = "requestQueue";
+
+    private final ManagedChannel channel;
+    private final MessageServiceGrpc.MessageServiceBlockingStub blockingStub;
+
+    public ConsumerApplication() {
+        // Создаем GRPC канал и клиент
+        this.channel = ManagedChannelBuilder.forAddress("localhost", 8080) // адрес вашего GRPC сервиса
+                .usePlaintext() // отключение шифрования для тестов
+                .build();
+        this.blockingStub = MessageServiceGrpc.newBlockingStub(channel);
+    }
 
     @Bean
     public Queue myQueue() {
@@ -18,6 +34,16 @@ public class ConsumerApplication {
     @RabbitListener(queues = QUEUE_NAME)
     public void listen(String message) {
         System.out.println("[" + QUEUE_NAME + "]: " + message);
+
+        RequestMessage request = RequestMessage.newBuilder()
+                .setText(message)
+                .build();
+
+        try {
+            blockingStub.sendMessage(request);
+        } catch (StatusRuntimeException e) {
+            System.err.println("GRPC RPC failed: " + e.getStatus());
+        }
     }
 
     public static void main(String[] args) {
